a3a629307753c83626b967a6e8573b5b8f4d1f0a
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-04-26 11:37:10 +0900

    Add files via upload

---
+++ WebClientComputing/lab1/output.js
@@ -0,0 +1,3 @@
+console.log("(output.js) console.log() -- Hello World! 이세현이다.");
+document.write("<h1>(output.js) document.write() -- 하하하, 여기도 이세현!!! </h1>");
+alert("(output.js) alert() -- 소프트웨어학부(20213043)");
