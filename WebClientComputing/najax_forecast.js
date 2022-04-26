5f6291e5a79cf745612fe9f2a4dff20af4c535e3
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-04-26 11:40:10 +0900

    Add files via upload

---
+++ WebClientComputing/najax_forecast.js
@@ -0,0 +1,38 @@
+﻿let najax = $ = require('najax');
+let fs=require('fs');
+
+
+function getData(callback) {
+	var tableData;
+	$.get('https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&lang=kr&appid=1e1edc8170eec8cad4c927ae8876cd3f', callback);
+    return tableData;
+}
+
+getData(function (response) {
+		tableData = response;
+    console.log(typeof(tableData));
+    console.log(tableData);
+    console.log('------------------------------------');
+
+    const output = JSON.parse(tableData);
+    console.log(typeof(output));
+    console.log(output);
+    console.log('------------------------------------');
+
+    console.log("도시: " + output.name);
+    console.log("국가: " + output.sys.country);
+    console.log("날씨: " + output.weather[0]["description"]);
+    console.log("평균온도: " + output.main["temp"] + " (°C)");
+    console.log("최저온도: " + output.main["temp_min"] + " (°C)");
+    console.log("최고온도: " + output.main["temp_max"] + " (°C)");
+    console.log("풍속: " + output.wind["speed"] + " (m/sec)");
+    console.log("습도: " + output.main["humidity"] + " (%)");
+
+/*    fs.writeFile(__dirname+"/temp/weather_seoul.json", tableData, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file(weather_seoul.json) was saved!");
+     });
+*/
+});
---
+++ WebClientComputing/request_forecast.js
@@ -0,0 +1,30 @@
+﻿let request=require('request');
+let fs=require('fs');
+
+console.log(__dirname);
+
+let homepage=request('http://api.openweathermap.org/data/2.5/forecast?q=Busan&units=metric&lang=kr&appid=1e1edc8170eec8cad4c927ae8876cd3f', function(err, res, body){
+    console.log(typeof(body)); // string
+    console.log(body);
+    console.log('------------------------------------');
+
+    const output = JSON.parse(body); // JSON 데이터(문자열)을 객체로 변환
+    console.log(typeof(output)); // object
+    console.log(output);
+    console.log('------------------------------------');
+
+    console.log("도시: " + output.city.name);
+    console.log("국가: " + output.city.country);
+
+    for (let i = 0; i < output.list.length; i++) {
+        console.log('------------------------------------');
+        let date = new Date(output.list[i].dt*1000);
+        console.log("예보일시: " + date.toString());
+        console.log("날씨: " + output.list[i].weather[0]["description"]);
+        console.log("평균온도: " + output.list[i].main["temp"] + " (°C)");
+        console.log("최저온도: " + output.list[i].main["temp_min"] + " (°C)");
+        console.log("최고온도: " + output.list[i].main["temp_max"] + " (°C)");
+        console.log("풍속: " + output.list[i].wind["speed"] + " (m/sec)");
+        console.log("습도: " + output.list[i].main["humidity"] + " (%)");
+    }
+});
