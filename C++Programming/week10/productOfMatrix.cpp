7bc9c1ad59228afdd88c9306bf7d204a2cbece93
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-05-12 14:48:14 +0900

    Add files via upload

--- C++Programming/week10/productOfMatrix.cpp
+++ C++Programming/week10/productOfMatrix.cpp
@@ -1,32 +1,25 @@
 /***********************
-홀수 마방진
+행렬곱셉
 20213043 이세현
 ***********************/
 #include <iostream>
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++){
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++){
         int r, s, t;
         cin >> r >> s >> t;
 
         int A[r][s];
         for (int x = 0; x < r; x++) {
             for (int y = 0; y < s; y++) cin >> A[x][y];
-        }
-        for (int x = 0; x < r; x++) {
-            for (int y = 0; y < s; y++) cout << A[x][y] << " ";
-            cout << endl;
-        }        
+        }    
         int B[s][t];
         for (int x = 0; x < s; x++) {
-            for (int y = 0; y < t; t++) cin >> B[x][y];
-        }
-        for (int x = 0; x < s; x++) {
-            for (int y = 0; y < t; t++) cin >> B[x][y];
-        }       
+            for (int y = 0; y < t; y++) cin >> B[x][y];
+        }   
 
         int answer[r][t];
         for (int x = 0; x < r; x++) {
@@ -39,9 +32,6 @@ int main() {
             }
             cout << endl;
         }
-        
-
-
     }
     return 0;
 }
