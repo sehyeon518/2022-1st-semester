7025d785247d40681c33b0571a57d6f6e86f6e85
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-10-21 13:04:00 +0900

    Add files via upload

---
+++ C++Programming/MyTerm.h
@@ -0,0 +1,34 @@
+#ifndef _MYTERM_H_
+#define _MYTERM_H_
+#include <iostream>
+
+using namespace std;
+
+class myPolynomial;
+
+class myTerm {
+    private:
+        int coeff;
+        unsigned exp;
+    public:
+        myTerm(int c = 0, unsigned e = 0);
+        myTerm(const myTerm& term);
+
+        bool operator == (const myTerm& term) const;
+        bool operator != (const myTerm& term) const;
+        bool operator < (const myTerm& term) const;
+
+        myTerm operator - () const;
+
+        int getCoeff() const { return coeff; }
+        unsigned getExp() const { return exp; }
+
+        void setCoeff(int c) { coeff = c; }
+        void setExp(unsigned e) { exp = e; }
+
+        myTerm ddx() const;
+
+        friend ostream& operator <<(ostream& outStream, const myTerm& term);
+        friend myPolynomial;
+};
+#endif _MYTERM_H_
